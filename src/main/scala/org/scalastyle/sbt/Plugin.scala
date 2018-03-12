e60375db5051f0b5c2939a0ebfb65deaa94ca0bc
Jason Steenstra-Pickens <jason@biomatters.com>
2018-03-12 17:21:23 +1300

    Add scalastyleCompileArgs setting

--- src/main/scala/org/scalastyle/sbt/Plugin.scala
+++ src/main/scala/org/scalastyle/sbt/Plugin.scala
@@ -53,7 +53,7 @@ object ScalastylePlugin extends AutoPlugin {
   object autoImport {
     val scalastyle = inputKey[Unit]("Run scalastyle on your code")
     val scalastyleGenerateConfig = taskKey[Unit]("Generate a default configuration files for scalastyle")
-    val compileThenCheckStyle = taskKey[Analysis]("Compiles sources and then runs scalastyle on your code.")
+    val compileThenCheckStyle = taskKey[Analysis]("Compiles sources and then runs scalastyle on your code")
 
     val scalastyleTarget = settingKey[File]("XML output file from scalastyle")
     val scalastyleConfig = settingKey[File]("Scalastyle configuration file")
@@ -63,6 +63,7 @@ object ScalastylePlugin extends AutoPlugin {
     val scalastyleConfigRefreshHours = settingKey[Integer]("How many hours until next run will fetch the scalastyle-config.xml again if location is a URI.")
     val scalastyleConfigUrlCacheFile = settingKey[String]("If scalastyleConfigUrl is set, it will be cached here")
     val scalastyleSources = settingKey[Seq[File]]("Which sources will scalastyle check")
+    val scalastyleCompileArgs = settingKey[Seq[String]]("The arguments that are passed to scalastyle when using compileThenCheckStyle")
   }
 
   import autoImport._
@@ -119,7 +120,9 @@ object ScalastylePlugin extends AutoPlugin {
       scalastyleFailOnWarning := false,
       (scalastyleFailOnWarning in Test) := (scalastyleFailOnWarning in scalastyle).value,
       scalastyleSources := (unmanagedSourceDirectories in Compile).value,
-      (scalastyleSources in Test) := (unmanagedSourceDirectories in Test).value
+      (scalastyleSources in Test) := (unmanagedSourceDirectories in Test).value,
+      scalastyleCompileArgs := Seq.empty,
+      (scalastyleCompileArgs in Test) := (scalastyleCompileArgs in scalastyle).value
     ) ++
     Project.inConfig(Compile)(rawScalastyleSettings()) ++
     Project.inConfig(Test)(rawScalastyleSettings())
